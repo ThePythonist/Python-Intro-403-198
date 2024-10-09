bbcac735c0b63bad74bc6c682380a1a894e78b98
arash <awrash.sn@gmail.com>
2024-10-09 07:24:10 -0700

    S22

--- 94.py
+++ 94.py
@@ -1,3 +1,6 @@
+from tkinter import ttk, messagebox
+
+
 def main():
     end = False
     board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -23,21 +26,88 @@ def main():
         ))
 
     def choice1():
-        return int(input("Choose : "))
+        while True:
+            place = input("Player 1 Choice : ")
+            try:
+                place = int(place)
+                place = place - 1
+
+                if place in range(0, 9):
+                    if board[place] in ["X", "Y"]:
+                        # print("You can't go there. Try again")
+                        messagebox.showerror("Invalid Entry", "You can't go there. Try again")
+                    else:
+                        return place
+            except ValueError:
+                # print("Entry must be a number. Try again")
+                messagebox.showerror("Invalid Entry", "Entry must be a number. Try again")
 
     def choice2():
-        return int(input("Choose : "))
+        while True:
+            place = input("Player 2 Choice : ")
+            try:
+                place = int(place)
+                place = place - 1
+
+                if place in range(0, 9):
+                    if board[place] in ["X", "Y"]:
+                        # print("You can't go there. Try again")
+                        messagebox.showerror("Invalid Entry", "You can't go there. Try again")
+                    else:
+                        return place
+            except ValueError:
+                # print("Entry must be a number. Try again")
+                messagebox.showerror("Invalid Entry", "Entry must be a number. Try again")
 
     def move1():
-        n = choice1()
+        choice = choice1()
+        board[choice] = "X"
 
     def move2():
-        n = choice2()
+        choice = choice2()
+        board[choice] = "Y"
 
     def check_board():
-        pass
+        count = 0
+
+        for i in win_combinations:
+            if board[i[0]] == board[i[1]] == board[i[2]] == "X":
+                # print("Player 1 Wins!")
+                messagebox.showinfo("End", "Player 1 Wins!")
+                return True
+            elif board[i[0]] == board[i[1]] == board[i[2]] == "Y":
+                # print("Player 2 Wins!")
+                messagebox.showinfo("End", "Player 2 Wins!")
+                return True
+
+        for i in board:
+            if i in ["X", "Y"]:
+                count += 1
+
+        if count == 9:
+            # print("The game ends level")
+            messagebox.showwarning("End", "The game ends level")
+            return True
 
     while not end:
         table()
+        end = check_board()
+
+        if end:
+            break
+
+        move1()
+        table()
+        end = check_board()
+
+        if end:
+            break
+
+        move2()
+        table()
+
+    if input("Play again ? (y/n) : ").lower() == "y":
+        main()
+
 
 main()
